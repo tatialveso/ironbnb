907f221666258e348a3c232c9e7dc033bd792e79
tatialveso <tatiialveso@gmail.com>
2022-11-14 13:47:56 -0300

    :tada: set finished project

--- src/components/ApartmentList/ApartmentList.js
+++ src/components/ApartmentList/ApartmentList.js
@@ -8,21 +8,6 @@ function ApartmentList() {
     const [apartments, setApartments] = useState([])
     const [fetching, setFetching] = useState(true)
 
-    // captura a API (as informações de dados)
-    // fetch("https://ironbnb-m3.herokuapp.com/apartments")
-    //     // se deu certo então transforme esses dados em .json()
-    //     .then((response) => {
-    //         return response.json();
-    //     })
-    //     // então pegue os dados e imprima no console
-    //     .then((data) => {
-    //         console.log("Resposta: ", data)
-    //     })
-    //     .catch((error) => console.log(error))
-
-    // callback -> o código que a gente vai rodar
-    // array de valores que o código vai depender para rodar
-        // se o array estiver vazio, o useEffect roda quando a aplicação iniciada
     useEffect(() => {
         axios.get("https://ironbnb-m3.herokuapp.com/apartments")
         .then((response) => {
@@ -52,7 +37,7 @@ function ApartmentList() {
                 </Row>
                 <Row>
                     { fetching && <Spinner animation="border" /> }
-                    { renderApartments }
+                    { renderApartments.reverse() }
                 </Row>
             </Container>
         </div>
@@ -60,15 +45,3 @@ function ApartmentList() {
 }
 
 export default ApartmentList;
-
-
-
-// Promises -> promessas
-// promessa ser cumprida (então deu tudo certo) -> codigo segue fazendo algo
-// OU a promessa não ser cumprida (então deu erro) -> pegar erro pra analisar o que aconteceu
-
-
-// POST ->   CREATE -> ADICIONAMOS UM ITEM NOVO PRA API
-// GET ->    READ ->   LER OS ITENS DA NOSSA API
-// PUT ->    UPDATE -> ATUALIZA UM ITEM EXISTENTE COM NOVAS INFORMAÇÕES
-// DELETE -> DELETE -> EXCLUI UM ITEM DA API

9530da11aa980565071285cc3e60d868183d7a0a
tatialveso <tatiialveso@gmail.com>
2022-11-14 12:37:14 -0300

    :sparkles: finish class

--- src/components/AddApartment/AddApartment.js
+++ src/components/AddApartment/AddApartment.js
@@ -1,12 +1,32 @@
+import axios from "axios";
 import { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
 
 function AddApartment() {
     const [show, setShow] = useState(false);
+    const [headline, setHeadline] = useState("")
+    const [price, setPrice] = useState(1)
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-  
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const body = {
+            title: headline,
+            pricePerDay: price
+        }
+
+        axios.post("https://ironbnb-m3.herokuapp.com/apartments", body)
+        .then((response) => {
+            setHeadline("")
+            setPrice(1)
+        })
+
+        setShow(false)
+    }
+
     return (
         <div className="my-3">
             <Button variant="info" onClick={handleShow}>
@@ -18,13 +38,15 @@ function AddApartment() {
                     <Modal.Title>Cadastrar apartamento</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <Form>
+                    <Form onSubmit={ handleSubmit }>
                         <Form.Group className="mb-3">
                             <Form.Label>Título do anúncio</Form.Label>
                             <Form.Control
                                 type="text"
                                 placeholder="Insira um título"
                                 name="headline"
+                                value={ headline }
+                                onChange={ (e) => setHeadline(e.target.value) }
                             />
                         </Form.Group>
                         <Form.Group className="mb-3">
@@ -33,6 +55,8 @@ function AddApartment() {
                                 type="number"
                                 placeholder="Insira o preço por dia"
                                 name="pricePerDay"
+                                value={ price }
+                                onChange={ (e) => setPrice(e.target.value) }
                             />
                         </Form.Group>
                         <Button type="submit">Cadastrar</Button>
